3b73d3b3ff194c375418ddcb1bbea1eb68fc82a4
TanjinaQ <tanjina.feroz@qestit.se>
2024-10-03 15:31:12 +0200

    added creds json in fixtures folder

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,10 +6,13 @@ describe('visit aurdev', () => {
     });
 
     it('can login credentials', () => {
-      //sign in with the test-credentials
-      cy.get('#input-69').type('aur@bth.se'); 
-      cy.get('#input-72').type('demo123');
-      cy.contains('button', 'Sign in').click();
+      
+      cy.fixture('credentials').then((creds) => {
+        // Use the loaded credentials
+        cy.get('#input-69').type(creds.email);
+        cy.get('#input-72').type(creds.password);
+        cy.contains('button', 'Sign in').click();
+      });
       
       //Make your Menu is what displays on the page when a user is signed in
       cy.contains('h1', 'Make Your Menu!').should('be.visible');
